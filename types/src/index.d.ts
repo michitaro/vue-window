8a71342fc59492f46659ab2a4b84a871a01b94b3
michitaro <michitaro.nike@gmail.com>
2017-11-18 16:20:27 +0900

    Updates

---
+++ types/src/index.d.ts
@@ -0,0 +1,7 @@
+import { StyleBlack, StyleWhite, StyleMetal, StyleFactory } from './style';
+import Vue from 'vue';
+export { WindowType } from "./window/script";
+export { StyleBlack, StyleWhite, StyleMetal, StyleFactory };
+export declare function install(vue: typeof Vue, options?: {
+    prefix: string;
+}): void;
