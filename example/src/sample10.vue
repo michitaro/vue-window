5efdbe8a77046144c528526d210d7fa472b990c2
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2018-08-21 12:00:19 +0800

    changed sample

--- example/src/sample10.vue
+++ example/src/sample10.vue
@@ -3,7 +3,10 @@
 
         <hsc-window
             title="Window 1"
+            :closeButton="true"
             :collapseButton="true"
+            :resizable="true"
+            :isOpen.sync="isOpen"
             :isCollapsed.sync="isCollapsed">
             Parameters:
             <fieldset>
@@ -14,9 +17,13 @@
                 <legend>&beta;</legend>
                 <input type="range" />
             </fieldset>
+
+            <div slot="footer">
+              <button @click="isOpen = false">Close</button>
+            </div>
         </hsc-window>
 
-        <button @click="isCollapsed = ! isCollapsed">Toggle Window 1</button>
+        <button @click="isOpen = ! isOpen">Toggle Window 1</button>
 
     </hsc-window-style-metal>
 </template>
@@ -25,7 +32,8 @@
 export default <any>{
     data() {
         return {
-            isCollapsed: false,
+            isOpen: true,
+            isCollapsed: false
         }
     },
 }
