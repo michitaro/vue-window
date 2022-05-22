eba9b4946b1d30f8f3742ba31d2fac45fd547dc2
Voltra <ludwig.guerin.98@gmx.fr>
2022-05-22 15:32:11 +0200

    Migrate to UMD and extract CSS to avoid unnecessary conflicts with SSR

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
     entry: [
@@ -7,17 +8,23 @@ module.exports = {
     output: {
         path: `${__dirname}/lib`,
         filename: 'index.js',
-        libraryTarget: 'commonjs',
+        libraryTarget: 'umd',
+		library: 'VueWindow',
+		umdNamedDefine: true,
+		globalObject: 'typeof self !== \'undefined\' ? self : typeof window !== \'undefined\' ? window : this',
     },
     resolve: {
         extensions: ['.ts', '.js'],
     },
     module: {
         rules: [
-            { test: /\.vue$/, use: 'vue-loader' },
-            { test: /\.scss/, use: ["style-loader", "css-loader", "sass-loader",] },
+            { test: /\.vue$/, use: 'vue-loader', exclude: /node_modules/i, },
+            { test: /\.scss/, use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader",] },
             {
-                test: /\.ts$/, loader: 'ts-loader', options: {
+                test: /\.ts$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/i,
+				options: {
                     appendTsSuffixTo: [/\.vue$/],
                     compilerOptions: {
                         declaration: true,
@@ -28,15 +35,16 @@ module.exports = {
             }
         ],
     },
-    externals: [
+    /*externals: [
         // include only relative assets
         function ({ context, request }, callback) {
             if (!request.match(/(?:^|!)(?:\.|\.\.)?\//))
                 return callback(null, `commonjs ${request}`)
             callback()
         }
-    ],
+    ],*/
     plugins: [
+		new MiniCssExtractPlugin(),
         new VueLoaderPlugin(),
     ],
 }
